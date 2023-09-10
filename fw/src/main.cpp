370ea0b309b4e7c5ac3e5a242e6a7a25f1a03955
chase <tccox3@knights.ucf.edu>
2023-09-09 20:35:01 -0500

    fw fixed loop, carry over from last version

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -173,23 +173,5 @@ void loop()
 
   // Check if data is available to read from the serial port
   if (Serial.available())
-  {
     serial_handler();
-    int c = Serial.read();
-    unsigned long start_time = millis();
-    while (c >= 0 && c != '\n' && c != '\r' && strlen(incomingData) < MAX_BUFFER_SIZE - 1 && millis() - start_time < SERIAL_TIMEOUT)
-    {
-      incomingData[strlen(incomingData)] = c;
-      c = Serial.read();
-    }
-
-    if (strlen(incomingData) > 0)
-    {
-      command_handler(incomingData);
-    }
-
-    // reset the buffer
-    for (size_t i = 0; i < MAX_BUFFER_SIZE; i++)
-      incomingData[i] = '\0';
-  }
 }
