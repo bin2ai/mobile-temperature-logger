ce26fde6bbde445f92c87e1dd59c30d0492a410a
chase <tccox3@knights.ucf.edu>
2023-09-30 11:53:50 -0500

    rename class members
    tk prefix for tk objects, serial, etc.

--- sw/mtl_gui.py
+++ sw/mtl_gui.py
@@ -14,24 +14,26 @@ from tkinter import messagebox
 
 class SerialConnectApp:
 
-    root: tk.Tk
+    # GUI elements
+    tk_root: tk.Tk
+    tk_label_port: ttk.Label
+    tk_combobox_port: ttk.Combobox
+    tk_button_connect: ttk.Button
+    tk_label_status: ttk.Label
+    tk_timer: tk.Tk
+    tk_label_timer: ttk.Label
+    tk_button_timer: ttk.Button
+    tk_label_delay: ttk.Label
+    tk_slider_delay: ttk.Scale
+    tk_label_period: ttk.Label
+    tk_scale_period: ttk.Scale
+    tk_label_samples: ttk.Label
+    tk_button_reset: ttk.Button
+    # Serial port
     serial_port: serial.Serial
-    port_label: ttk.Label
-    port_combo: ttk.Combobox
-    connect_button: ttk.Button
-    status_label: ttk.Label
+    # other
     timer_interval: int
-    timer: tk.Tk
-    timer_label: ttk.Label
-    timer_button: ttk.Button
-    timer_running: bool
-    delay_label: ttk.Label
-    delay_slider: ttk.Scale
-    period_label: ttk.Label
-    period_slider: ttk.Scale
-    samples_label: ttk.Label
-    reset_button: ttk.Button
-
+    is_timer_running: bool
     is_connected: bool = False
     timer_interval_device_state = 5000
 
@@ -46,9 +48,9 @@ class SerialConnectApp:
 
     device_state: int = 0
 
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Mobile Temperature Logger (MTL)")
+    def __init__(self, root: tk.Tk):
+        self.tk_root = root
+        self.tk_root.title("Mobile Temperature Logger (MTL)")
 
         # a ? button that opens up a url to the github page
         self.help_button = ttk.Button(
@@ -56,23 +58,23 @@ class SerialConnectApp:
         self.help_button.pack(padx=10, pady=5)
         self.serial_port = None
 
-        self.port_label = ttk.Label(root, text="Select COM Port:")
-        self.port_label.pack(padx=10, pady=5)
+        self.tk_label_port = ttk.Label(root, text="Select COM Port:")
+        self.tk_label_port.pack(padx=10, pady=5)
 
-        self.port_combo = ttk.Combobox(root, state="readonly")
-        self.port_combo.pack(padx=10, pady=5)
+        self.tk_combobox_port = ttk.Combobox(root, state="readonly")
+        self.tk_combobox_port.pack(padx=10, pady=5)
 
         # Bind the refresh_ports function to the <FocusIn> event of the combobox
-        self.port_combo.bind("<FocusIn>", self.refresh_ports)
+        self.tk_combobox_port.bind("<FocusIn>", self.refresh_ports)
 
-        self.connect_button = ttk.Button(
+        self.tk_button_connect = ttk.Button(
             root, text="Connect", command=self.toggle_connection)
-        self.connect_button.pack(padx=10, pady=5)
+        self.tk_button_connect.pack(padx=10, pady=5)
         # Initially disable the button
-        self.connect_button["state"] = "disabled"
+        self.tk_button_connect["state"] = "disabled"
 
-        self.status_label = ttk.Label(root, text="Status: Not Connected")
-        self.status_label.pack(padx=10, pady=5)
+        self.tk_label_status = ttk.Label(root, text="Status: Not Connected")
+        self.tk_label_status.pack(padx=10, pady=5)
 
         # horizontal line
         ttk.Separator(root, orient=tk.HORIZONTAL).pack(fill=tk.X)
@@ -82,38 +84,40 @@ class SerialConnectApp:
         self.settings_label.pack(padx=10, pady=5)
 
         # text 'there are 1000 samples, so a period of 1000 seconds is ~16 minutes'
-        self.samples_label = ttk.Label(
+        self.tk_label_samples = ttk.Label(
             root, text="There are 1000 samples,\nso a period of 1000 seconds is ~16 minutes")
-        self.samples_label.pack(padx=10, pady=5)
+        self.tk_label_samples.pack(padx=10, pady=5)
 
         # i want sliders that set the delay seconds (0-255), period (1-65535), and samples (1-30)
         # i want a row where text, then slider, not text for one row, slider for next row
-        self.delay_label = ttk.Label(root, text="Delay (seconds):")
-        self.delay_label.pack(padx=10, pady=5)
-        self.delay_slider = ttk.Scale(
+        self.tk_label_delay = ttk.Label(root, text="Delay (seconds):")
+        self.tk_label_delay.pack(padx=10, pady=5)
+        self.tk_slider_delay = ttk.Scale(
             root, from_=0, to=255, orient=tk.HORIZONTAL, command=self.set_button_function)
-        self.delay_slider.pack(padx=10, pady=5)
-        self.delay_slider.set(60)
-        self.delay_slider.bind("<ButtonPress-1>", self.on_slider_drag_start)
-        self.delay_slider.bind("<ButtonRelease-1>", self.on_slider_drag_stop)
+        self.tk_slider_delay.pack(padx=10, pady=5)
+        self.tk_slider_delay.set(60)
+        self.tk_slider_delay.bind("<ButtonPress-1>", self.on_slider_drag_start)
+        self.tk_slider_delay.bind(
+            "<ButtonRelease-1>", self.on_slider_drag_stop)
 
-        self.period_label = ttk.Label(root, text="Period (seconds):")
-        self.period_label.pack(padx=10, pady=5)
-        self.period_slider = ttk.Scale(
+        self.tk_label_period = ttk.Label(root, text="Period (seconds):")
+        self.tk_label_period.pack(padx=10, pady=5)
+        self.tk_scale_period = ttk.Scale(
             root, from_=1, to=100, orient=tk.HORIZONTAL, command=self.set_button_function)
-        self.period_slider.pack(padx=10, pady=5)
-        self.period_slider.set(60)
-        self.period_slider.bind("<ButtonPress-1>", self.on_slider_drag_start)
+        self.tk_scale_period.pack(padx=10, pady=5)
+        self.tk_scale_period.set(60)
+        self.tk_scale_period.bind("<ButtonPress-1>", self.on_slider_drag_start)
         # estimated time to finish text (period * 1000 + delay in seconds)
-        self.period_slider.bind("<ButtonRelease-1>", self.on_slider_drag_stop)
+        self.tk_scale_period.bind(
+            "<ButtonRelease-1>", self.on_slider_drag_stop)
         # show hrs,min, seconds
         self.est_time_label = ttk.Label(root, text="Estimated Time:")
         self.est_time_label.pack(padx=10, pady=5)
 
         # set button and reset button on the same row
-        self.reset_button = ttk.Button(
+        self.tk_button_reset = ttk.Button(
             root, text="Reset", command=self.reset_button_function)
-        self.reset_button.pack(padx=10, pady=5)
+        self.tk_button_reset.pack(padx=10, pady=5)
 
         # horizontal line
         ttk.Separator(root, orient=tk.HORIZONTAL).pack(fill=tk.X)
@@ -131,7 +135,7 @@ class SerialConnectApp:
         # progress bar (where 4.2 is full and 3.2 is empty)
         self.battery_progress = ttk.Progressbar(
             root, orient=tk.HORIZONTAL, length=200, mode="determinate", maximum=4.2, value=0)
-        
+
         self.battery_progress.pack(padx=10, pady=5)
 
         # text for estimated time
@@ -152,12 +156,12 @@ class SerialConnectApp:
             root, text="Start", command=self.start_button_function)
         self.start_button.pack(padx=10, pady=5)
 
-        #progress bar (where 1000 is full and 0 is empty)
+        # progress bar (where 1000 is full and 0 is empty)
         self.progress_collection = ttk.Progressbar(
             root, orient=tk.HORIZONTAL, length=200, mode="determinate", maximum=1000, value=0)
         self.progress_collection.pack(padx=10, pady=5)
 
-        #estimated time left text
+        # estimated time left text
         self.est_time_left_label = ttk.Label(root, text="Estimated Time Left:")
         self.est_time_left_label.pack(padx=10, pady=5)
 
@@ -176,9 +180,9 @@ class SerialConnectApp:
         self.check_and_refresh()
 
         # Start the timer to periodically check if connected and refresh COM ports
-        self.root.after(self.timer_interval, self.check_and_refresh)
+        self.tk_root.after(self.timer_interval, self.check_and_refresh)
 
-        self.root.after(5000, self.update_device_state)
+        self.tk_root.after(5000, self.update_device_state)
 
     def open_github(self):
         webbrowser.open("https://github.com/bin2ai/mobile-temperature-logger")
@@ -426,7 +430,6 @@ class SerialConnectApp:
                 self.battery_label["text"] = f"{vbat_voltage:.2f}V"
                 # update progress bar
                 self.battery_progress["value"] = vbat_voltage
-                                
 
                 # get AT+CHRG
                 self.serial_port.write(b'AT+CHRG\r\n')
@@ -458,36 +461,33 @@ class SerialConnectApp:
                 self.battery_info_label["text"] = battery_info
                 # Update the start button text based on the current state
 
-                #get index. AT+INDEX#
+                # get index. AT+INDEX#
                 self.serial_port.write(b'AT+TEMP#\r\n')
                 response = self.serial_port.readline().decode()
                 if "OK" not in response:
                     raise Exception(f"Error getting index: {response}")
                 # assume response is "OK+INDEX=[0-999]\r\n"
                 index = int(response.split("=")[1].split("\r")[0])
-                #update progress_collection
+                # update progress_collection
                 self.progress_collection["value"] = index
                 self.progress_collection["maximum"] = 999
                 self.progress_collection["length"] = 200
                 self.progress_collection["mode"] = "determinate"
                 self.progress_collection["orient"] = "horizontal"
 
-                #get the period and multiply by 999-index to get the time remaining
-                #est_time_left_label
+                # get the period and multiply by 999-index to get the time remaining
+                # est_time_left_label
                 self.serial_port.write(b'AT+PERIOD?\r\n')
                 response = self.serial_port.readline().decode()
                 if "OK" not in response:
                     raise Exception(f"Error getting index: {response}")
                 # assume response is "OK+INDEX=[0-999]\r\n"
                 period = int(response.split("=")[1].split("\r")[0])
-                #update est_time_left_label
+                # update est_time_left_label
                 est_time_left = (1000-index)*period
-                #HH:MM:SS
-                self.est_time_left_label["text"] = f"Estimated time left: {est_time_left//3600} Hr(s):{(est_time_left%3600)//60} Min(s):{est_time_left%60} Sec(s)"
-
-
-
-
+                # HH:MM:SS
+                self.est_time_left_label[
+                    "text"] = f"Estimated time left: {est_time_left//3600} Hr(s):{(est_time_left%3600)//60} Min(s):{est_time_left%60} Sec(s)"
 
             except Exception as e:
                 messagebox.showerror(
@@ -517,8 +517,8 @@ class SerialConnectApp:
             self.start_button["state"] = "normal"
 
         # Re-schedule the timer to check the device state again
-        self.root.after(self.timer_interval_device_state,
-                        self.update_device_state)
+        self.tk_root.after(self.timer_interval_device_state,
+                           self.update_device_state)
 
     def on_slider_drag_start(self, event):
         self.slider_dragging = True
@@ -535,24 +535,24 @@ class SerialConnectApp:
             # send serial command to device
             try:
                 self.serial_port.write(
-                    b'AT+DELAY=' + str(self.delay_slider.get()).encode() + b'\r\n')
+                    b'AT+DELAY=' + str(self.tk_slider_delay.get()).encode() + b'\r\n')
                 response = self.serial_port.readline().decode()
                 if "OK" not in response:
                     raise Exception(f"Error setting delay: {response}")
                 # split = and use the second part as delay
                 delay = response.split("=")[1].split("\r")[0]
                 # update delay label text
-                self.delay_label["text"] = f"Delay: {delay} seconds"
+                self.tk_label_delay["text"] = f"Delay: {delay} seconds"
 
                 self.serial_port.write(
-                    b'AT+PERIOD=' + str(self.period_slider.get()).encode() + b'\r\n')
+                    b'AT+PERIOD=' + str(self.tk_scale_period.get()).encode() + b'\r\n')
                 response = self.serial_port.readline().decode()
                 if "OK" not in response:
                     raise Exception(f"Error setting period: {response}")
 
                 period = response.split("=")[1].split("\r")[0]
 
-                self.period_label["text"] = f"Period: {period} seconds"
+                self.tk_label_period["text"] = f"Period: {period} seconds"
 
                 self.serial_port.write(
                     b'AT+SMPLS=30\r\n')
@@ -572,24 +572,24 @@ class SerialConnectApp:
         if not self.is_connected:
             # disable start, set, and reset buttons and slides
             self.start_button["state"] = "disabled"
-            self.reset_button["state"] = "disabled"
-            self.delay_slider["state"] = "disabled"
-            self.period_slider["state"] = "disabled"
+            self.tk_button_reset["state"] = "disabled"
+            self.tk_slider_delay["state"] = "disabled"
+            self.tk_scale_period["state"] = "disabled"
             self.download_button["state"] = "disabled"
         else:
             # enable start, set, and reset buttons and slides
             self.start_button["state"] = "normal"
-            self.reset_button["state"] = "normal"
-            self.delay_slider["state"] = "normal"
-            self.period_slider["state"] = "normal"
+            self.tk_button_reset["state"] = "normal"
+            self.tk_slider_delay["state"] = "normal"
+            self.tk_scale_period["state"] = "normal"
             self.download_button["state"] = "normal"
 
             self.state_label["text"] = f"{self.device_states[self.device_state]}"
             # if device is running, stopped, or cleared, only allow the user to edit in idle state
             if self.device_state in [1, 2, 3, 4]:
-                self.reset_button["state"] = "disabled"
-                self.delay_slider["state"] = "disabled"
-                self.period_slider["state"] = "disabled"
+                self.tk_button_reset["state"] = "disabled"
+                self.tk_slider_delay["state"] = "disabled"
+                self.tk_scale_period["state"] = "disabled"
 
             if self.device_state == 1:
                 self.start_button["text"] = "Start"
@@ -604,29 +604,29 @@ class SerialConnectApp:
 
     def reset_button_function(self):
         # make sliders go back to their default values
-        self.delay_slider.set(60)
-        self.period_slider.set(60)
+        self.tk_slider_delay.set(60)
+        self.tk_scale_period.set(60)
 
     def refresh_ports(self, event=None):
         # Get a list of available COM ports and update the dropdown
         available_ports = [port.device for port in list_ports.comports()]
-        self.port_combo["values"] = available_ports
+        self.tk_combobox_port["values"] = available_ports
 
     def check_and_refresh(self):
         # Check if the serial port is connected
         if self.serial_port and self.serial_port.is_open:
-            self.status_label["text"] = "Status: Connected"
-            self.connect_button["text"] = "Disconnect"
+            self.tk_label_status["text"] = "Status: Connected"
+            self.tk_button_connect["text"] = "Disconnect"
             # Enable the button for disconnecting
-            self.connect_button["state"] = "normal"
+            self.tk_button_connect["state"] = "normal"
         else:
-            self.status_label["text"] = "Status: Not Connected"
-            self.connect_button["text"] = "Connect"
+            self.tk_label_status["text"] = "Status: Not Connected"
+            self.tk_button_connect["text"] = "Connect"
             # Enable the button for connecting
-            self.connect_button["state"] = "normal"
+            self.tk_button_connect["state"] = "normal"
 
         # Check if the selected COM port is still available
-        selected_port = self.port_combo.get()
+        selected_port = self.tk_combobox_port.get()
         available_ports = [port.device for port in list_ports.comports()]
         if selected_port not in available_ports:
             self.disconnect_serial()
@@ -635,7 +635,7 @@ class SerialConnectApp:
         self.refresh_ports()
 
         # Re-schedule the timer to check again after the timer_interval
-        self.root.after(self.timer_interval, self.check_and_refresh)
+        self.tk_root.after(self.timer_interval, self.check_and_refresh)
 
         self.update_state()
 
@@ -646,7 +646,7 @@ class SerialConnectApp:
             self.connect_serial()
 
     def connect_serial(self):
-        selected_port = self.port_combo.get()
+        selected_port = self.tk_combobox_port.get()
 
         if not selected_port:
             self.is_connected = False
